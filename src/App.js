f7fb76c04530ee4b118a10888487074f10764af1
alisha-nas <al.alishanasir@gmail.com>
2024-06-14 12:15:44 -0600

    Created component files for sections of the page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './shared-components/navbar';
 import HomePage from './pages/Homepage';
+import Contact from './pages/Contact';
+import Experience from './pages/Experience';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+
 
 
 function App() {
@@ -9,6 +14,10 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
       <HomePage></HomePage>
+      <About></About>
+      <Experience></Experience>
+      <Portfolio></Portfolio>
+      <Contact></Contact>
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,16 @@
-// In the homepage, I want
+/*
+  This page will be used as the landing page component. This contains the greeting and 
+  animated svgs that the user will be directed to when they click my page. 
+*/ 
 
+import './Homepage.css'
 
 export default function HomePage() { 
   return (
-    <h1>Hello</h1>
+    <div class = "container">
+      <div class = "typewriter">
+        <h1>Hi, I'm Alisha</h1>
+      </div>
+    </div>
   )
 }
