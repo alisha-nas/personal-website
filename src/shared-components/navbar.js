a54cd38fa784dd349ab2587b87230436be92b555
alisha-nas <al.alishanasir@gmail.com>
2024-07-03 14:37:53 -0600

    Added correct functionality to nav bar

--- src/shared-components/navbar.js
+++ src/shared-components/navbar.js
@@ -1,23 +1,25 @@
-import './navbar.css'
-import { Link } from 'react-router-dom';
+import './navbar.css';
+import { Link } from 'react-scroll';
 
-export default function Navbar () { 
-  return ( 
-    <nav class = "navBar">
-      <ul class = "navLinks">
-      <li>  
-          <Link to="about-section" spy={true} smooth={true} offset={50} duration={500}>About</Link>
-      </li>
-      <li> 
-          <Link to="experience-section" spy={true} smooth={true} offset={50} duration={500}>Experience</Link>
+const Navbar = () => {
+  return (
+    <nav className="navBar">
+      <ul className="navLinks">
+        <li>
+          <Link to="about-section" spy={true} smooth={true} offset={-50} duration={500}>About</Link>
         </li>
-        <li> 
-          <Link to="portfolio-section" spy={true} smooth={true} offset={50} duration={500}>Portfolio</Link>
-      </li>      
-      <li> 
-        <Link to="contact-section" spy={true} smooth={true} offset={50} duration={500}>Contact</Link>
-      </li>
+        <li>
+          <Link to="experience-section" spy={true} smooth={true} offset={-50} duration={500}>Experience</Link>
+        </li>
+        <li>
+          <Link to="portfolio-section" spy={true} smooth={true} offset={-50} duration={500}>Portfolio</Link>
+        </li>
+        <li>
+          <Link to="contact-section" spy={true} smooth={true} offset={-50} duration={500}>Contact</Link>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
+
+export default Navbar;
