0d94ff283ac896e8096e736106d12ea425652eef
Alisha Nasir <al.alishanasir@gmail.com>
2025-03-02 21:09:18 -0700

    Updated experience timeline content

--- src/pages/experience/Experience.js
+++ src/pages/experience/Experience.js
@@ -6,18 +6,25 @@ const Experience = () => {
     <section id="experience-section" className="experience-container">
       <h1>Experience</h1>
       <div className="timeline">
-        <div className="timeline-item left">
+      <div className="timeline-item right">
+          <div className="timeline-content">
+            <h3>Incoming Software Developer Intern</h3>
+            <p className="experience-date">06/25 - 08/25</p>
+            <p className="experience-description">HubSpot</p>
+          </div>
+        </div>
+      <div className="timeline-item left">
           <div className="timeline-content">
             <h3>Software Developer Intern</h3>
-            <p className="experience-date">05/24 - 08/24</p>
-            <p className="experience-description">Bell</p>
+            <p className="experience-date">01/24 - 04/24</p>
+            <p className="experience-description">RBC</p>
           </div>
         </div>
         <div className="timeline-item right">
           <div className="timeline-content">
-            <h3>Programming Instructor</h3>
-            <p className="experience-date">10/23 - Present</p>
-            <p className="experience-description">While She is True</p>
+            <h3>Software Developer Intern</h3>
+            <p className="experience-date">05/24 - 08/24</p>
+            <p className="experience-description">Bell</p>
           </div>
         </div>
         <div className="timeline-item left">
